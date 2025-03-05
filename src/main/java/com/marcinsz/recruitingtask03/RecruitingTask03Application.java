3e9511fd35bb1dd2126e185cffc30f6e7284d345
MarcinSz1993 <marcinsz1993@hotmail.com>
2025-03-05 18:10:13 +0100

    Initial commit

---
+++ src/main/java/com/marcinsz/recruitingtask03/RecruitingTask03Application.java
@@ -0,0 +1,13 @@
+package com.marcinsz.recruitingtask03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitingTask03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitingTask03Application.class, args);
+	}
+
+}
